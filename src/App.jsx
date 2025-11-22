96a922a8237e2125bd8f59c2a2d1bc7fa347fe19
SECHANG1412 <binaryblitz1412@gmail.com>
2025-11-22 23:42:14 +0900

    Context 분리하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,45 @@
-import "./App.css";
-import {
-  useRef,
-  useState,
-  useReducer,
-  useCallback,
-  createContext,
-} from "react";
-import Header from "./components/Header";
-import Editor from "./components/Editor";
-import List from "./components/List";
+import './App.css';
+import { useRef, useState, useReducer, useCallback, createContext, useMemo } from 'react';
+import Header from './components/Header';
+import Editor from './components/Editor';
+import List from './components/List';
 
 const mockData = [
   {
     id: 0,
     isDone: false,
-    content: "React 공부하기",
+    content: 'React 공부하기',
     date: new Date().getTime(),
   },
   {
     id: 1,
     isDone: false,
-    content: "빨래하기",
+    content: '빨래하기',
     date: new Date().getTime(),
   },
   {
     id: 2,
     isDone: false,
-    content: "노래 연습하기",
+    content: '노래 연습하기',
     date: new Date().getTime(),
   },
 ];
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CREATE":
+    case 'CREATE':
       return [action.data, ...state];
-    case "UPDATE":
-      return state.map((item) =>
-        item.id === action.targetId
-          ? { ...item, isDone: !item.isDone }
-          : item
-      );
-    case "DELETE":
+    case 'UPDATE':
+      return state.map((item) => (item.id === action.targetId ? { ...item, isDone: !item.isDone } : item));
+    case 'DELETE':
       return state.filter((item) => item.id !== action.targetId);
     default:
       return state;
   }
 }
 
-export const TodoContext = createContext();
+export const TodoStateContext = createContext();
+export const TodoDispatchContext = createContext();
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
@@ -56,7 +47,7 @@ function App() {
 
   const onCreate = useCallback((content) => {
     dispatch({
-      type: "CREATE",
+      type: 'CREATE',
       data: {
         id: idRef.current++,
         isDone: false,
@@ -68,32 +59,31 @@ function App() {
 
   const onUpdate = useCallback((targetId) => {
     dispatch({
-      type: "UPDATE",
+      type: 'UPDATE',
       targetId: targetId,
     });
   }, []);
 
   const onDelete = useCallback((targetId) => {
     dispatch({
-      type: "DELETE",
+      type: 'DELETE',
       targetId: targetId,
     });
+  }, []);
+
+  const memoizedDispatch = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
   }, []);
 
   return (
     <div className="App">
       <Header />
-      <TodoContext.Provider
-        value={{
-          todos,
-          onCreate,
-          onUpdate,
-          onDelete,
-        }}
-      >
-        <Editor />
-        <List />
-      </TodoContext.Provider>
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider value={memoizedDispatch}>
+          <Editor />
+          <List />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </div>
   );
 }
